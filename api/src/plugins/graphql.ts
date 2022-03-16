b9a9001a1c0dfb285aaf4d423862a4d0d993d59f
Yaodong Zhao <git@yaodong.org>
2022-03-15 22:31:27 -0700

    Revert "add graphql"
    This reverts commit ade45f3ba9606a1aeee6109a92a25e7eeebfa0da.

--- api/src/plugins/graphql.ts
+++
@@ -1,9 +0,0 @@
-import fp from "fastify-plugin";
-import mercurius from "mercurius";
-import { schema, resolvers } from "../graphql";
-
-export interface GraphQLOptions {}
-
-export default fp<GraphQLOptions>(async (fastify, opts) => {
-  fastify.register(mercurius, { schema, resolvers });
-});
